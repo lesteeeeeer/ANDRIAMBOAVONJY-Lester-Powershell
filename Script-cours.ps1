944dfa8fb42c39058bcf8d718794451218b78e44
unknown <Administrateur@ISITECH.LOCAL>
2026-05-29 10:26:06 +0200

    Changement d'un fichier

--- Script-cours.ps1
+++ Script-cours.ps1
@@ -1,3 +1,30 @@
+Add-Type -AssemblyName System.Windows.function 
+Add-Type -AssemblyName System.Drawing 
+# creation fenetre
+$form = New-Object System.Windows.Forms.Form
+$form.Text = "Configuration Serveur"
+$form.StartPosition = "CenterScreen"
+
+# creation bouton
+$buttonDhcp = New-Object System.Windows.Forms.Button
+$buttonDhcp.Text = "Ajout DHCP"
+$buttonDhcp.Add_Click({
+        [string]$scopeName = 'IsitechLocal'
+        [string]$start = '192.168.31.40'
+        [string]$stop = '192.168.31.90'
+        [string]$mask = '255.255.255.0'
+        [int]$day = 8
+        [string]$dns = [Microsoft.VisualBasic.Interaction]::InputBox("Veuillez sasair le dns", "DHCP", "192.168.100.100")
+        [string]$domain = 'isitech.local'
+        [string]$router = '192.168.31.254'
+
+        Install-WindowsFeature -Name DHCP -IncludeManagementTools -Restart:$false -Verbose
+        Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\ServerManager\Roles\12' -Name ConfigurationState -Value 2
+        Add-DhcpServerv4Scope -Name $scopeName -StartRange $start -EndRange $stop -SubnetMask $mask -LeaseDuration (New-TimeSpan -Days $day) -State Active
+        Set-DhcpServerv4OptionValue -DnsServer $dns -DnsDomain $domain -Router $router
+})
+
+
 do {
     Write-Host "========================================="
     Write-Host "================== MENU ================="
@@ -5,7 +32,7 @@ do {
     Write-Host "1 - Ajout et configuration du role DHCP"
     Write-Host "2 - Renommer le serveur"
     Write-Host "3 - Installer et configurer AD DS"
-
+    
     [int]$choix = Read-Host "Veuillez faire un choix"
 
     if ($choix -eq 1) {
